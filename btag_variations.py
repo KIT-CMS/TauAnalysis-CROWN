ebc1259edc42235c1a3a33ab8e07d620f3cc82de
Artur Monsch <60860160+a-monsch@users.noreply.github.com>
2025-06-27 15:29:41 +0200

    Adding a more compact representation of variation information with usage of a custom wrapper

--- btag_variations.py
+++ btag_variations.py
@@ -1,175 +1,20 @@
-from code_generation.systematics import SystematicShift
 from .producers import scalefactors as scalefactors
+from .scripts.CROWNWrapper import defaults, get_adjusted_add_shift_SystematicShift
 
 
-def add_btagVariations(configuration):
-    #########################
-    # btagging shape uncertainties
-    #########################
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncHFUp",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "up_hf"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
+def add_btagVariations(configuration):  # adding btagging shape uncertainties
+    add_shift = get_adjusted_add_shift_SystematicShift(configuration)  # shift_config=(scopes: {shift_key: shift_map[direction]})
+    with defaults(
+        scopes=("mt", "et", "tt"),
+        shift_key="btag_sf_variation",
         exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncHFDown",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "down_hf"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncHFstats1Up",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "up_hfstats1"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncHFstats1Down",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "down_hfstats1"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncHFstats2Up",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "up_hfstats2"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncHFstats2Down",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "down_hfstats2"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncLFUp",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "up_lf"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncLFDown",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "down_lf"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncLFstats1Up",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "up_lfstats1"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncLFstats1Down",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "down_lfstats1"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncLFstats2Up",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "up_lfstats2"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncLFstats2Down",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "down_lfstats2"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncCFerr1Up",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "up_cferr1"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncCFerr1Down",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "down_cferr1"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncCFerr2Up",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "up_cferr2"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncCFerr2Down",
-            shift_config={
-                ("mt", "et", "tt"): {"btag_sf_variation": "down_cferr2"},
-            },
-            producers={("mt", "et", "tt"): scalefactors.btagging_SF},
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
+        producers=[scalefactors.btagging_SF],
+    ):
+        add_shift(name="btagUncHF", shift_map={"Up": "up_hf", "Down": "down_hf"})
+        add_shift(name="btagUncHFstats1", shift_map={"Up": "up_hfstats1", "Down": "down_hfstats1"})
+        add_shift(name="btagUncHFstats2", shift_map={"Up": "up_hfstats2", "Down": "down_hfstats2"})
+        add_shift(name="btagUncLF", shift_map={"Up": "up_lf", "Down": "down_lf"})
+        add_shift(name="btagUncLFstats1", shift_map={"Up": "up_lfstats1", "Down": "down_lfstats1"})
+        add_shift(name="btagUncLFstats2", shift_map={"Up": "up_lfstats2", "Down": "down_lfstats2"})
+        add_shift(name="btagUncCFerr1", shift_map={"Up": "up_cferr1", "Down": "down_cferr1"})
+        add_shift(name="btagUncCFerr2", shift_map={"Up": "up_cferr2", "Down": "down_cferr2"})
---
+++ scripts/CROWNWrapper.py
@@ -0,0 +1,245 @@
+from __future__ import annotations  # needed for type annotations in > python 3.7
+
+import contextvars
+import inspect
+import re
+from contextlib import contextmanager
+from contextvars import ContextVar
+from typing import Any, Callable, Dict, Generator, List, Tuple, Union
+
+from code_generation.configuration import Configuration, TConfiguration
+from code_generation.producer import (
+    BaseFilter,
+    ExtendedVectorProducer,
+    Filter,
+    Producer,
+    ProducerGroup,
+    TProducerInput,
+    VectorProducer,
+)
+from code_generation.quantity import QuantitiesInput
+from code_generation.rules import ProducerRule
+from code_generation.systematics import SystematicShift
+
+
+CONTEXT_REGISTRY: Dict[str, contextvars.ContextVar] = {
+    k: contextvars.ContextVar(k, default=None)
+    for k in [
+        "scopes",
+        "shift_key",
+        "shift_map",
+        "producers",
+        "ignore_producers",
+        "samples",
+        "exclude_samples",
+        "input",
+        "subproducers",
+        "call",
+        "output",
+        "vec_configs",
+    ]
+}
+
+
+@contextmanager
+def defaults(**kwargs: Any) -> Generator[None, None, None]:
+    tokens = []
+    try:
+        for key, value in kwargs.items():
+            try:
+                tokens.append(CONTEXT_REGISTRY[key].set(value))
+            except KeyError:
+                raise ValueError(f"Unknown context variable: {key}")
+        yield
+    finally:
+        for token in reversed(tokens):
+            token.var.reset(token)
+
+
+def get_adjusted_add_shift_SystematicShift(configuration: Configuration) -> Callable:
+    def add_shift(
+        name: str,
+        # those can be set via the context manager defaults
+        scopes: Union[str, Tuple[str, ...]] = None,
+        shift_key: str = None,
+        shift_map: Dict[str, Any] = None,
+        producers: Union[Dict[Union[str, Tuple[str, ...]], List[object]], List[object]] = None,
+        ignore_producers: Union[None, Dict[str, Any]] = None,
+        samples: Union[str, List[str], None] = None,
+        exclude_samples: Union[str, List[str], None] = None,
+        # this can be set explicetly to capture more complex shift configurations (toghter with producers)
+        shift_config: Union[Dict[str, Any], None] = None,
+    ):
+        # if shift_config is set it overrides shift_key and shift_map
+        # if producers is procided as a dict it ignores scopes
+
+        scopes = scopes or CONTEXT_REGISTRY["scopes"].get()
+        shift_key = shift_key or CONTEXT_REGISTRY["shift_key"].get()
+        shift_map = shift_map or CONTEXT_REGISTRY["shift_map"].get()
+        producers = producers or CONTEXT_REGISTRY["producers"].get()
+
+        to_be_checked = (
+            [shift_config, producers]
+            if shift_config is not None and isinstance(producers, dict)
+            else [scopes, shift_key, shift_map, producers]
+        )
+
+        if any(it is None for it in to_be_checked):
+            raise ValueError(
+                "scopes, shift_key, shift_map, and producers must be set."
+            )
+
+        for direction, value in shift_map.items() if shift_map else shift_config.items():
+            configuration.add_shift(
+                SystematicShift(
+                    name=f"{name}{direction}",
+                    shift_config=shift_config[direction] if shift_config else {
+                        scopes: (
+                            dict(zip(shift_key, value))
+                            if all(
+                                [
+                                    isinstance(shift_key, (list, tuple)),
+                                    isinstance(value, (list, tuple)),
+                                    len(shift_key) == len(value),
+                                ]
+                            )
+                            else {shift_key: value}
+                        )
+                    },
+                    producers=producers if isinstance(producers, dict) else {scopes: producers},
+                    ignore_producers=ignore_producers or CONTEXT_REGISTRY["ignore_producers"].get() or {},
+                ),
+                samples=samples or CONTEXT_REGISTRY["samples"].get(),
+                exclude_samples=exclude_samples or CONTEXT_REGISTRY["exclude_samples"].get(),
+            )
+    return add_shift
+
+
+def _get_variable_name():
+    frame = inspect.currentframe().f_back.f_back
+    code_context = inspect.getframeinfo(frame).code_context
+    if code_context:
+        call_line = code_context[0].strip()
+        match = re.match(r"([\w\d_]+)\s*=", call_line)
+        if match:
+            return match.group(1)
+    raise RuntimeError("Could not determine variable name from context")
+
+
+class MissingValue(Exception):
+    def __init__(self, variable_name: str):
+        super().__init__(
+            f"Missing value for variable '{variable_name}'. "
+            "Please provide a value either as an argument or "
+            "through a 'with defaults(...)' context."
+        )
+
+
+class NameNotDetermined(Exception):
+    def __init__(self):
+        super().__init__(
+            "Name could not be determined. "
+            "This should not happened. Workaround: provide the name explicitly"
+        )
+
+
+class AutoBaseFilter(BaseFilter):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("name", _get_variable_name())
+        kwargs.setdefault("input", CONTEXT_REGISTRY["input"].get())
+        kwargs.setdefault("scopes", CONTEXT_REGISTRY["scopes"].get())
+
+        if kwargs["name"] is None:
+            raise NameNotDetermined
+
+        for key in ["scopes", "input"]:
+            if kwargs[key] is None:
+                raise MissingValue(key)
+
+        super().__init__(*args, **kwargs)
+
+
+class AutoFilter(Filter):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("name", _get_variable_name())
+        kwargs.setdefault("input", CONTEXT_REGISTRY["input"].get())
+        kwargs.setdefault("scopes", CONTEXT_REGISTRY["scopes"].get())
+        kwargs.setdefault("subproducers", CONTEXT_REGISTRY["subproducers"].get())
+
+        if kwargs["name"] is None:
+            raise NameNotDetermined
+
+        for key in ["scopes", "input", "subproducers"]:
+            if kwargs[key] is None:
+                raise MissingValue(key)
+
+        super().__init__(*args, **kwargs)
+
+
+class AutoProducer(Producer):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("name", _get_variable_name())
+        kwargs.setdefault("input", CONTEXT_REGISTRY["input"].get())
+        kwargs.setdefault("scopes", CONTEXT_REGISTRY["input"].get())
+
+        if kwargs["name"] is None:
+            raise NameNotDetermined
+
+        for key in ["scopes", "input"]:
+            if kwargs[key] is None:
+                raise MissingValue(key)
+
+        super().__init__(*args, **kwargs)
+
+
+class AutoProducerGroup(ProducerGroup):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("name", _get_variable_name())
+        kwargs.setdefault("call", CONTEXT_REGISTRY["call"].get())
+        kwargs.setdefault("input", CONTEXT_REGISTRY["input"].get())
+        kwargs.setdefault("scopes", CONTEXT_REGISTRY["input"].get())
+
+        if kwargs["name"] is None:
+            raise NameNotDetermined
+
+        for key in ["scopes"]:
+            if kwargs[key] is None:
+                raise MissingValue(key)
+
+        super().__init__(*args, **kwargs)
+
+
+class AutoVectorProducer(VectorProducer):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("name", _get_variable_name())
+        kwargs.setdefault("input", CONTEXT_REGISTRY["input"].get())
+        kwargs.setdefault("output", CONTEXT_REGISTRY["output"].get())
+        kwargs.setdefault("scopes", CONTEXT_REGISTRY["input"].get())
+        kwargs.setdefault("vec_configs", CONTEXT_REGISTRY["vec_configs"].get())
+
+        if kwargs["name"] is None:
+            raise NameNotDetermined
+
+        for key in ["scopes", "input", "vec_configs"]:
+            if kwargs[key] is None:
+                raise MissingValue(key)
+
+        super().__init__(*args, **kwargs)
+
+
+class AutoExtendedVectorProducer(ExtendedVectorProducer):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("name", _get_variable_name())
+        kwargs.setdefault("input", CONTEXT_REGISTRY["input"].get())
+        kwargs.setdefault("output", CONTEXT_REGISTRY["output"].get())
+        kwargs.setdefault("scopes", CONTEXT_REGISTRY["input"].get())
+        kwargs.setdefault("vec_configs", CONTEXT_REGISTRY["vec_configs"].get())
+
+        if kwargs["name"] is None:
+            raise NameNotDetermined
+
+        for key in ["scopes", "input", "output", "vec_configs"]:
+            if kwargs[key] is None:
+                raise MissingValue(key)
+
+        super().__init__(*args, **kwargs)
