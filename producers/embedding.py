3d7b8441d410a3ce6a941e1c35ed084bf70e6152
Sebastian <urote@stuent.kit.edu>
2026-03-10 08:56:10 +0100

    Removed old producers

--- producers/embedding.py
+++ producers/embedding.py
@@ -10,9 +10,9 @@ with defaults(scopes=["et", "mt", "tt", "em", "mm", "ee"]):
         TauEmbeddingInitialMETphi = Producer(input=[nanoAOD.TauEmbedding_initialMETphi], output=[q.emb_initialMETphi])
         TauEmbeddingInitialPuppiMETEt = Producer(input=[nanoAOD.TauEmbedding_initialPuppiMETEt], output=[q.emb_initialPuppiMETEt])
         TauEmbeddingInitialPuppiMETphi = Producer(input=[nanoAOD.TauEmbedding_initialPuppiMETphi], output=[q.emb_initialPuppiMETphi])
-        TauEmbeddingnInitialPairCandidates = Producer(input=[nanoAOD.TauEmbedding_nInitialPairCandidates], output=[q.emb_InitialPairCandidates])
-        TauEmbeddingSelectionOldMass = Producer(input=[nanoAOD.TauEmbedding_SelectionOldMass], output=[q.emb_SelectionOldMass])
-        TauEmbeddingSelectionNewMass = Producer(input=[nanoAOD.TauEmbedding_SelectionNewMass], output=[q.emb_SelectionNewMass])
+        #TauEmbeddingnInitialPairCandidates = Producer(input=[nanoAOD.TauEmbedding_nInitialPairCandidates], output=[q.emb_InitialPairCandidates])
+        #TauEmbeddingSelectionOldMass = Producer(input=[nanoAOD.TauEmbedding_SelectionOldMass], output=[q.emb_SelectionOldMass])
+        #TauEmbeddingSelectionNewMass = Producer(input=[nanoAOD.TauEmbedding_SelectionNewMass], output=[q.emb_SelectionNewMass])
 
     with defaults(call="event::quantity::Rename<Bool_t>({df}, {output}, {input})"):
         TauEmbeddingIsMediumLeadingMuon = Producer(input=[nanoAOD.TauEmbedding_isMediumLeadingMuon], output=[q.emb_isMediumLeadingMuon])
